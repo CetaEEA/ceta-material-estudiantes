4afa6db18920fded9b4fcd7a803ebfc8633bda75
CetaEEA <64902696+CetaEEA@users.noreply.github.com>
2026-09-20 19:47:43 -0400

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,28 @@
+// ============================================================
+// CETA - MATERIAL ACADÉMICO
+// CONEXIÓN A SUPABASE
+// ============================================================
 
+
+// ------------------------------------------------------------
+// IMPORTANTE
+//
+// Coloca aquí EXACTAMENTE la misma URL y ANON KEY
+// que utilizas en Material de Gabinete.
+//
+// NO colocar service_role.
+// ------------------------------------------------------------
+
+const SUPABASE_URL = "https://cystgztmeyinsvmkkbji.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_4n5HfbFA8otKAg_X3ic8ig_VZzCqgH1";
+
+
+// ------------------------------------------------------------
+// CREAR CLIENTE
+// ------------------------------------------------------------
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
